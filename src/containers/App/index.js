661131b58e3fa2ae1cb33bf6936657f2c249c0c8
Adam <acdoyle630@gmail.com>
2017-10-03 17:30:41 -0700

    onMount and button runs loadavg()

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './styles.css';
+var os = require("os")
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+  }
+
+  //Mount cpu average
+
+  componentWillMount() {
+    //os.loadavg()
+    console.log(os.loadavg())
+
+  }
+
+  checkLoadAvg(){
+    console.log(os.loadavg())
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -13,6 +33,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick = {this.checkLoadAvg}> click </button>
       </div>
     );
   }
