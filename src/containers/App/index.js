71c1aa8a8d2f7198a7d25240a77a05fdb9adccaa
Adam <acdoyle630@gmail.com>
2017-10-03 21:58:23 -0700

    line chart to visualize load

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import LineChart from 'react-linechart';
 import logo from './logo.svg';
 import './styles.css';
 var os = require("os")
@@ -7,6 +8,12 @@ class App extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      valueOne: 0,
+      valueTwo: 0,
+      valueThree: 0
+    }
 
   }
 
@@ -23,7 +30,7 @@ class App extends Component {
 
   }
 
-  checkLoadAvg(){
+  checkLoadAvg =()=>{
     console.log('clicked')
     fetch('/api/loadAvg', {
       method: "GET",
@@ -31,12 +38,28 @@ class App extends Component {
     }).then((response) =>{
       return response.json()
     }).then((values) =>{
-      console.log(values)
+      this.loadNewValues(values)
+    })
+  }
+
+  loadNewValues=(values)=>{
+    this.setState({
+      valueOne : values[0],
+      valueTwo : values[1],
+      valueThree : values[2]
     })
   }
 
 
   render() {
+    console.log(this.state)
+
+    const data = [
+    {
+      color: "blue",
+      points: [{x: 1, y: this.state.valueOne}, {x: 3, y: this.state.valueTwo}, {x: 5, y: this.state.valueThree}]
+    }]
+
     return (
       <div className="App">
         <header className="App-header">
@@ -46,7 +69,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <button onClick = {this.checkLoadAvg}> click </button>
+        <button onClick = {this.checkLoadAvg}> click
+        </button>
+        <LineChart
+          width={600}
+          height={400}
+          yMin = {0.5}
+          yMax = {2.8}
+          data={data}
+        />
       </div>
     );
   }
