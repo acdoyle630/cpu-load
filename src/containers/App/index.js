9e0d3f7579e16813b48face9069d9fd03b674a71
Adam <acdoyle630@gmail.com>
2017-10-04 10:38:34 -0700

    changed values to an array from an object

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -13,19 +13,10 @@ class App extends Component {
     this.state = {
         selectedIndex: 0,
         selectedInterval:"One Minute",
-        valueOne: 0,
-        valueTwo: 0,
-        valueThree: 0,
-        valueFour : 0,
-        valueFive : 0,
-        valueSix : 0,
-        valueSeven : 0,
-        valueEight : 0,
-        valueNine : 0
+        valueArray :[],
+        displaySpeed: 0
       }
   }
-
-  //Mount cpu average
 
   componentWillMount() {
     setInterval(() => {
@@ -36,7 +27,8 @@ class App extends Component {
 
   chooseZero=()=>{
     this.setState({
-      selectedIndex: 0
+      selectedIndex: 0,
+      selectedInterval: "One Minute"
     })
   }
   chooseOne=()=>{
@@ -52,7 +44,7 @@ class App extends Component {
     })
   }
 
-  checkLoadAvg =()=>{
+  checkLoadAvg=()=>{
     fetch('/api/loadAvg', {
       method: "GET",
       credentials: "include"
@@ -63,74 +55,30 @@ class App extends Component {
     })
   }
 
-  loadNewValues(myValues){
-    if(this.state.valueNine !==0 && this.state.valueOne!==0 && this.state.valueTwo!==0 && this.state.valueThree!==0 && this.state.valueFour !== 0 && this.state.valueFive !== 0 && this.state.valueSix !== 0 && this.state.valueSeven !== 0 && this.state.valueEight !== 0){
+  loadNewValues(returnedValues){
+    let alteredValueArray = []
+    if(this.state.valueArray.length < 9){
       this.setState({
-        valueOne: this.state.valueTwo,
-        valueTwo: this.state.valueThree,
-        valueThree: this.state.valueFour,
-        valueFour: this.state.valueFive,
-        valueFive: this.state.valueSix,
-        valueSix: this.state.valueSeven,
-        valueSeven: this.state.valueEight,
-        valueEight: this.state.valueNine,
-        valueNine: myValues[this.state.selectedIndex]
+        valueArray: [...this.state.valueArray, returnedValues[this.state.selectedIndex]],
+        displaySpeed: returnedValues[this.state.selectedIndex]
       })
     }
-    if(this.state.valueNine ===0 && this.state.valueOne!==0 && this.state.valueTwo!==0 && this.state.valueThree!==0 && this.state.valueFour !== 0 && this.state.valueFive !== 0 && this.state.valueSix !== 0 && this.state.valueSeven !== 0 && this.state.valueEight !== 0){
+    else{
+      alteredValueArray = this.state.valueArray
+      alteredValueArray.shift()
+      alteredValueArray.push(returnedValues[this.state.selectedIndex])
       this.setState({
-        valueNine: myValues[this.state.selectedIndex]
-      })
-    }
-    if(this.state.valueEight === 0 && this.state.valueOne!==0 && this.state.valueTwo!==0 && this.state.valueThree!==0 && this.state.valueFour !== 0 && this.state.valueFive !== 0 && this.state.valueSix !== 0 && this.state.valueSeven !== 0){
-      this.setState({
-        valueEight: myValues[this.state.selectedIndex]
-      })
-    }
-    if(this.state.valueSeven === 0 && this.state.valueOne!==0 && this.state.valueTwo!==0 && this.state.valueThree!==0 && this.state.valueFour !== 0 && this.state.valueFive !== 0 && this.state.valueSix !== 0){
-      this.setState({
-        valueSeven: myValues[this.state.selectedIndex]
-      })
-    }
-    if(this.state.valueSix === 0 && this.state.valueOne!==0 && this.state.valueTwo!==0 && this.state.valueThree!==0 && this.state.valueFour !== 0 && this.state.valueFive !== 0){
-      this.setState({
-        valueSix: myValues[this.state.selectedIndex]
-      })
-    }
-    if(this.state.valueFive ===0 && this.state.valueOne!==0 && this.state.valueTwo!==0 && this.state.valueThree!==0 && this.state.valueFour !== 0){
-      this.setState({
-        valueFive: myValues[this.state.selectedIndex]
-      })
-    }
-    if(this.state.valueFour ===0 && this.state.valueOne!==0 && this.state.valueTwo!==0 && this.state.valueThree!==0){
-      this.setState({
-        valueFour: myValues[this.state.selectedIndex]
-      })
-    }
-    if(this.state.valueThree === 0 && this.state.valueTwo !==0 && this.state.valueOne !== 0){
-      this.setState({
-        valueThree: myValues[this.state.selectedIndex]
-      })
-    }
-    if(this.state.valueTwo === 0 && this.state.valueOne !== 0){
-      this.setState({
-        valueTwo: myValues[this.state.selectedIndex]
-      })
-    }
-    if(this.state.valueOne === 0){
-      this.setState({
-        valueOne: myValues[this.state.selectedIndex]
+        valueArray: alteredValueArray,
+        displaySpeed: returnedValues[this.state.selectedIndex]
       })
     }
   }
-
-
 
   render() {
     const data = [
     {
       color: "blue",
-      points: [{x: 1, y: this.state.valueOne}, {x: 2, y: this.state.valueTwo}, {x: 3, y: this.state.valueThree}, {x: 4, y: this.state.valueFour}, {x:5, y:this.state.valueFive}, {x:6, y:this.state.valueSix}, {x:7, y: this.state.valueSeven}, {x:8, y:this.state.valueEight}, {x:9, y:this.state.valueNine}]
+      points: [{x: 1, y: this.state.valueArray[0]}, {x: 2, y: this.state.valueArray[1]}, {x: 3, y: this.state.valueArray[2]}, {x: 4, y: this.state.valueArray[3]}, {x:5, y:this.state.valueArray[4]}, {x:6, y:this.state.valueArray[5]}, {x:7, y: this.state.valueArray[6]}, {x:8, y:this.state.valueArray[7]}, {x:9, y:this.state.valueArray[8]}]
     }
     ]
 
@@ -141,7 +89,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          selected interval: {this.state.selectedInterval}, {this.state.valueNine}
+          selected interval: {this.state.selectedInterval}, {this.state.displaySpeed}
         </p>
         <button onClick = {this.chooseZero}> One Minute Interval
         </button>
@@ -152,14 +100,14 @@ class App extends Component {
         <ReactSpeedometer
           minValue = {0}
           maxValue = {3}
-          value = {this.state.valueNine}
+          value = {this.state.displaySpeed}
         />
         <LineChart
-          width={600}
-          height={400}
-          yMin = {0.5}
-          yMax = {3}
-          data={data}
+           width={600}
+           height={400}
+           yMin = {0}
+           yMax = {3}
+           data={data}
         />
       </div>
     );
