990f2a343b8ac392b5a507f57448693002621103
Adam <acdoyle630@gmail.com>
2017-10-04 10:46:18 -0700

    started readme markdown

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -86,7 +86,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">CPU Load</h1>
         </header>
         <p className="App-intro">
           selected interval: {this.state.selectedInterval}, {this.state.displaySpeed}
